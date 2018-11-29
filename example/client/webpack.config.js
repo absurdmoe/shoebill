faa9c4e92e99311201ce25f5d39aeb85e06db11d
mchugtai <mchugtai@guildmortgage.net>
2018-11-29 00:02:23 -0800

    update client

---
+++ example/client/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // creates index.html file for us in the compiled folder, this will be created when we create the compiled folder after running webpack
+
+module.exports = {
+    entry: __dirname + '/source/index.js',
+    output: {
+        path: path.join(__dirname,'/compiled'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './source/index.html'
+        })
+    ]
+}
