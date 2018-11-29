e43679d858f6a7524fd1a701f98173b0afa6825f
mchugtai <mchugtai@guildmortgage.net>
2018-11-28 22:32:50 -0800

    updated example to include airbnb config and client and server

--- example/server/index.js
+++ example/server/index.js
@@ -1,3 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => {
+  console.error('listening on 3000');
+});
