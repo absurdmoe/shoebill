572ffb365e94b0218bd1d02586807e6db7abe397
mchugtai <mchugtai@guildmortgage.net>
2018-11-29 17:34:53 -0800

    fixed node_modules

--- example/client/webpack.config.js
+++ example/client/webpack.config.js
@@ -2,29 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // creates index.html file for us in the compiled folder, this will be created when we create the compiled folder after running webpack
 
 module.exports = {
-    entry: __dirname + '/source/index.js',
-    output: {
-        path: path.join(__dirname,'/compiled'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './source/index.html'
-        })
-    ]
-}
+  entry: path.join(__dirname, '/source/index.js'),
+  output: {
+    path: path.join(__dirname, '/compiled'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/source/index.html'),
+    }),
+  ],
+};
